ef9800635bff612ebcb83d2a4bb0c5f03bff7401
Tommy Chu <tommychu2256@gmail.com>
2020-08-15 16:43:00 +0200

    define the user struct

---
+++ accounts/models/user.go
@@ -0,0 +1,14 @@
+package models
+
+// Account defines all stored variables for an account
+type Account struct {
+	ID        string
+	Email     string
+	Username  string
+	HPassword string
+	FirstName string
+	LastName  string
+	BirthDay  string
+	CreatedAt string
+	UpdatedAt string
+}
