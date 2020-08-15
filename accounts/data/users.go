4910f2fc903aa4c0a9e847f6076f4e2be7c5a2a1
Tommy Chu <tommychu2256@gmail.com>
2020-08-15 16:43:09 +0200

    define the database interface

---
+++ accounts/data/users.go
@@ -0,0 +1,26 @@
+package data
+
+import (
+	"context"
+
+	"github.com/chutified/appointments/accounts/models"
+)
+
+// Database is an interface, which is able to make the database actions.
+type Database interface {
+
+	// Create
+	AddAccount(ctx context.Context, u *models.Account)
+
+	// Read
+	GetAccountsAll(ctx context.Context, pageCap int, pageNum int)
+	GetAccountByID(ctx context.Context, id string)
+	GetAccountByParams(ctx context.Context, u *models.Account)
+	LoginAccount(ctx context.Context, email string, hPasswd string)
+
+	// Update
+	EditAccountById(ctx context.Context, id string)
+
+	// Delete
+	DeleteAccountById(ctx context.Context, id string)
+}
