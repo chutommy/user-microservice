2155f1e463d45da85d35201cc14ba5b573a5deb5
Tommy Chu <tommychu2256@gmail.com>
2020-08-16 14:36:14 +0200

    seperate (estetic stuffs)

--- accounts/models/user.go
+++ accounts/models/user.go
@@ -5,11 +5,14 @@ type Account struct {
 	ID        string
 	Username  string
 	HPassword string
-	Email     string
-	Phone     string
+
+	Email string
+	Phone string
+
 	FirstName string
 	LastName  string
 	BirthDay  string
+
 	CreatedAt string
 	UpdatedAt string
 	DeletedAt string
