347f14b50a07cfb37d9170ad090e1a9238caec9c
Tommy Chu <tommychu2256@gmail.com>
2020-08-16 21:42:48 +0200

    implement account's handlers

---
+++ accounts/controllers/accounts.go
@@ -0,0 +1,18 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) AddAccount(c *gin.Context) {
+}
+func (h *Handler) GetAccountsAll(c *gin.Context) {
+}
+func (h *Handler) GetAccountByID(c *gin.Context) {
+}
+func (h *Handler) GetAccountByParams(c *gin.Context) {
+}
+func (h *Handler) LoginAccount(c *gin.Context) {
+}
+func (h *Handler) EditAccountByID(c *gin.Context) {
+}
+func (h *Handler) DeleteAccountByID(c *gin.Context) {
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/chutified/appointments
 
 go 1.15
 
-require github.com/pkg/errors v0.9.1
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/pkg/errors v0.9.1
+)
