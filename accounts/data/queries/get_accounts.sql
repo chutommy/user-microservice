cc776a20c5f50c4ba6cf4d1381ea740a9db2e53f
Tommy Chu <tommychu2256@gmail.com>
2020-08-18 21:26:38 +0200

    remove the password from the responses

--- accounts/data/queries/get_accounts.sql
+++ accounts/data/queries/get_accounts.sql
@@ -1,4 +1,4 @@
-SELECT id, username, email, phone, hpassword, first_name, last_name, birth_day, perm_address, mail_address, created_at, updated_at
+SELECT id, username, email, phone, first_name, last_name, birth_day, perm_address, mail_address, created_at, updated_at
 FROM accounts
 WHERE deleted_at == NULL
 ORDER BY $3 $4
--- accounts/data/queries/get_by_id.sql
+++ accounts/data/queries/get_by_id.sql
@@ -1,4 +1,4 @@
-SELECT id, username, email, phone, hpassword, first_name, last_name, birth_day, perm_address, mail_address, created_at, updated_at
+SELECT id, username, email, phone, first_name, last_name, birth_day, perm_address, mail_address, created_at, updated_at
 FROM accounts
 WHERE id == $1 AND deleted_at == NULL
 LIMIT 1;
