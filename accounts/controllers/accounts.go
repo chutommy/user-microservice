48220d286591c2d6e163429224c62ae320d41ffa
Tommy Chu <tommychu2256@gmail.com>
2020-08-21 11:58:29 +0200

    add AddAccount handler

--- accounts/controllers/accounts.go
+++ accounts/controllers/accounts.go
@@ -1,9 +1,37 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/chutified/appointments/accounts/models"
+	"github.com/gin-gonic/gin"
+)
 
+// AddAccount adds a new account to database.
 func (h *Handler) AddAccount(c *gin.Context) {
+
+	// get the account
+	var a models.Account
+	err := c.BindJSON(&a)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// add to database
+	id, err := h.ds.AddAccount(c, &a)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "success (" + id + ")",
+	})
 }
+
 func (h *Handler) GetAccountsAll(c *gin.Context) {
 }
 func (h *Handler) GetAccountByID(c *gin.Context) {
