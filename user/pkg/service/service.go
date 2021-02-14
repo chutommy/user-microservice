a114b1bdd0190fd1871def1bd2c4e007fb4c5e04
chutified <tommychu2256@gmail.com>
2021-02-14 20:13:47 +0100

    Add user service interface

---
+++ user/pkg/service/service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"context"
+	"time"
+)
+
+// UserService describes the service.
+type UserService interface {
+	AddGender(ctx context.Context, title string) (int64, error)
+	GetGender(ctx context.Context, id int64, title string) (Gender, error)
+	ListGenders(ctx context.Context) ([]Gender, error)
+	RemoveGender(ctx context.Context, title string) error
+
+	CreateUser(ctx context.Context, username, password, firstName, lastName, gender, email, phoneNumber string, birthDay time.Time) (User, error)
+	GetUserByID(ctx context.Context, id int64) (User, error)
+	GetUserByUsername(ctx context.Context, username string) (User, error)
+	GetUserByEmail(ctx context.Context, email string) (User, error)
+	UpdateUserUsername(ctx context.Context, id int64, username string) (User, error)
+	UpdateUserEmail(ctx context.Context, id int64, email string) (User, error)
+	UpdateUserPhoneNumber(ctx context.Context, id int64, phoneNumber string) (User, error)
+	UpdateUserPassword(ctx context.Context, id int64, password string) (User, error)
+	UpdateUserInfo(ctx context.Context, firstName, lastName, gender string, birthDay time.Time) (User, error)
+	DeleteUserSoft(ctx context.Context, id int64) error
+	DeleteUserPermanent(ctx context.Context, id int64) error
+	VerifyPassword(ctx context.Context, id int64, password string) error
+}
