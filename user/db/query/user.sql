3e9d8859a27543c196c1f8660cf8fc8028e4e2db
chutified <tommychu2256@gmail.com>
2021-02-20 18:44:50 +0100

    Reformat sql queries: lowercase consistency

--- user/db/query/user.sql
+++ user/db/query/user.sql
@@ -1,37 +1,37 @@
--- name: CreateUser :one
+-- name: createuser :one
 insert into users (email, hashed_password, first_name, last_name, birth_day, gender, phone_number)
 values ($1, $2, $3, $4, $5, $6, $7)
 returning *;
 
--- name: GetUserByID :one
+-- name: getuserbyid :one
 select *
 from users
 where id = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 limit 1;
 
--- name: GetUserByEmail :one
+-- name: getuserbyemail :one
 select *
 from users
 where email = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 limit 1;
 
--- name: UpdateUserEmail :one
+-- name: updateuseremail :one
 update users
 set email = $2
 where id = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 returning *;
 
--- name: UpdateUserPassword :one
+-- name: updateuserpassword :one
 update users
 set hashed_password = $2
 where id = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 returning *;
 
--- name: UpdateUserInfo :one
+-- name: updateuserinfo :one
 update users
 set first_name   = $2,
     last_name    = $3,
@@ -39,31 +39,31 @@ set first_name   = $2,
     gender       = $5,
     phone_number = $6
 where id = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 returning *;
 
--- name: DeleteUserSoft :exec
+-- name: deleteusersoft :exec
 update users
 set deleted_at = now()
 where id = $1
-  and deleted_at IS NULL;
+  and deleted_at is null;
 
--- name: RecoverDeletedUser :one
+-- name: recoverdeleteduser :one
 update users
 set deleted_at = null
 where id = $1
-  and deleted_at IS NOT NULL
+  and deleted_at is not null
 returning *;
 
--- name: DeleteUserPermanent :exec
+-- name: deleteuserpermanent :exec
 delete
 from users
 where id = $1
-  and deleted_at IS NULL;
+  and deleted_at is null;
 
--- name: GetHashedPassword :one
+-- name: gethashedpassword :one
 select hashed_password
 from users
 where id = $1
-  and deleted_at IS NULL
+  and deleted_at is null
 limit 1;
