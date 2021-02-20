3d713900e2b3bbf5aee1af7cfa24f51d16b3439e
chutified <tommychu2256@gmail.com>
2021-02-20 18:45:03 +0100

    Update service interface

--- user/pkg/service/service.go
+++ user/pkg/service/service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"time"
 
 	"user/pkg/repo"
 )
@@ -14,15 +13,15 @@ type UserService interface {
 	ListGenders(ctx context.Context) ([]repo.Gender, error)
 	RemoveGender(ctx context.Context, id int16) error
 
-	CreateUser(ctx context.Context, password, firstName, lastName string, gender int16, email, phoneNumber string, birthDay time.Time) (repo.User, error)
+	CreateUser(ctx context.Context, user repo.User) (repo.User, error)
 	GetUserByID(ctx context.Context, id int64) (repo.User, error)
 	GetUserByEmail(ctx context.Context, email string) (repo.User, error)
 	UpdateUserEmail(ctx context.Context, id int64, email string) (repo.User, error)
-	UpdateUserPhoneNumber(ctx context.Context, id int64, phoneNumber string) (repo.User, error)
 	UpdateUserPassword(ctx context.Context, id int64, password string) (repo.User, error)
-	UpdateUserInfo(ctx context.Context, firstName, lastName, gender string, birthDay time.Time) (repo.User, error)
+	UpdateUserInfo(ctx context.Context, id int64, user repo.User) (repo.User, error)
 	DeleteUserSoft(ctx context.Context, id int64) error
-	RecoverDeletedUser(ctx context.Context, id int64) (repo.User, error)
+	RecoverUser(ctx context.Context, id int64) (repo.User, error)
 	DeleteUserPermanent(ctx context.Context, id int64) error
+
 	VerifyPassword(ctx context.Context, id int64, password string) error
 }
