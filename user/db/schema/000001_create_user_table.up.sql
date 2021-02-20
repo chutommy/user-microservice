abde148b05e0a9f507f531318ee7d2a239454368
chutified <tommychu2256@gmail.com>
2021-02-20 18:28:31 +0100

    Modify up migration

--- user/db/schema/000001_create_user_table.up.sql
+++ user/db/schema/000001_create_user_table.up.sql
@@ -7,18 +7,19 @@ create table if not exists genders
 create table if not exists users
 (
     id              bigserial primary key,
+    email           varchar(64) not null unique,
     hashed_password varchar     not null,
     first_name      varchar(64) not null,
     last_name       varchar(64) not null,
     birth_day       date,
     gender          smallint,
-    email           varchar(64) not null unique,
     phone_number    varchar(32),
     updated_at      timestamptz,
     deleted_at      timestamptz,
     created_at      timestamptz not null default now()
 );
 
+-- update updated_at field before each update
 alter table users
     add foreign key (gender) references genders (id);
 
@@ -32,7 +33,6 @@ begin
     return new;
 end;
 $$;
-
 create trigger update_trigger
     before update
     on users
